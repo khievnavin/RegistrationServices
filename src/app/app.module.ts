bf40bf03b50f3fd360242c2ea605d67d87c9595b
Khiev Navin <khievnavin@gmail.com>
2025-01-23 09:02:51 +0700

    add: header

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    AppComponent,
+    CommonModule
+  ],
+  providers: [
+  
+  ],             // Add any services if needed
+})
+export class AppModule {}
